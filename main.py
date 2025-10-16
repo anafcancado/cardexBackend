36cca3a9a7e3a8d67a4923a348c020fe55943dd6
Ana Fernanda <anafcancado8@gmail.com>
2025-10-16 00:47:39 -0300

    teste render cloudflare

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 )
 
 # ✅ Coloque aqui o link do ngrok ativo do Colab
-COLAB_URL = "https://outstanding-singles-interest-sam.trycloudflare.com/predict"
+COLAB_URL = "https://calls-clear-mug-till.trycloudflare.com/predict"
 
 
 @app.post("/predict")
