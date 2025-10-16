cdbf9d2b1f767fc255012652da1bbcbdaf63fd96
Ana Fernanda <anafcancado8@gmail.com>
2025-10-16 09:27:45 -0300

    teste render cloudflare url4

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 )
 
 # ✅ Coloque aqui o link do ngrok ativo do Colab
-COLAB_URL = "https://specially-solve-hunter-duplicate.trycloudflare.com/predict"
+COLAB_URL = "https://excuse-generations-lived-lawyers.trycloudflare.com/predict"
 
 
 @app.post("/predict")
