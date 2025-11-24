eb253d04a68b268828aacd769c17ddc8525f4b50
Ana Fernanda <anafcancado8@gmail.com>
2025-11-24 09:45:38 -0300

    teste novo

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 )
 
 # ✅ Coloque aqui o link do ngrok ativo do Colab
-COLAB_URL = "https://demonstrate-remote-revealed-teach.trycloudflare.com/predict"
+COLAB_URL = "https://uniform-term-resolve-acute.trycloudflare.com/predict"
 
 
 @app.post("/predict")
